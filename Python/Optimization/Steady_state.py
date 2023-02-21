5dfe528e919504942a62e6ce177b9ac9accf8a58
Arcaru24601 <Jesper_holm@live.dk>
2023-02-21 15:01:52 +0100

    Test

--- Python/Optimization/Steady_state.py
+++ Python/Optimization/Steady_state.py
@@ -38,12 +38,12 @@ def cost_func(d15N_ref,d15n_model):
 
 
 
-model_path = '../CFM/CFM_main/CFMoutput/OptiNoise/CFMresults.hdf5.' ### Insert name
+model_path = '../CFM/CFM_main/CFMoutput/OptiNoise/CFMresults.hdf5' ### Insert name
 results_path = 'resultsFolder/minimizer.h5' #Add name
 
 
 # =============================================================================
-# Set parameters 
+# Set parameters
 # =============================================================================
 
 spin_year = 1000
@@ -74,7 +74,6 @@ Temp = np.full_like(Time,temp_0)
 
 Temp_csv = np.array([Time,Temp])
 #Bdot_csv = np.array([Time,Bdot])
-#os.chdir('../CFM/CFM_main/')
 #np.savetxt('../CFM/CFM_main/CFMinput/OptiNoise/optimize_acc.csv',Bdot_csv,delimiter=',')
 np.savetxt('../CFM/CFM_main/CFMinput/OptiNoise/optimize_temp.csv',Temp_csv,delimiter=',')
 
@@ -89,47 +88,50 @@ var_dict = {'count': np.zeros([N, 1], dtype=int),
 
 
 
-def func(temp,N_ref,var_dict):
-    
+def func(temp,N_ref,var_dict,path_to_model):
+
     count = int(np.max(var_dict['count']))
     print('Iteration',count)
-    
+
     i_temp = np.full_like(Time,temp)
     input_temp = np.array([Time,i_temp])
     i_acc = np.full(len(Time),expfunc(Beta, i_temp))
     input_acc = np.array([Time, i_acc])
-    
+
+    #os.chdir('../CFM/CFM_main')
+
+    np.savetxt('../CFM/CFM_main/CFMinput/OptiNoise/optimize_temp.csv', input_temp, delimiter=',')
+    np.savetxt('../CFM/CFM_main/CFMinput/OptiNoise/optimize_acc.csv', input_acc, delimiter=',')
+    #print(os.getcwd())
     os.chdir('../CFM/CFM_main')
-    
-    np.savetxt('CFMinput/OptiNoise/optimize_temp.csv', input_temp, delimiter=',')
-    np.savetxt('CFMinput/OptiNoise/optimize_acc.csv', input_acc, delimiter=',')
-    subprocess.run('python main.py FirnAir_Noise.json -n')
-    
+    #print(os.getcwd())
+    subprocess.run('python main.py FirnAir_Noise.json -n',shell=True)
     os.chdir('../../Optimization')
+    #os.chdir('../../Optimization')
+    print(os.getcwd())
     #print(model_path)
-    if os.path.exists(model_path):
-        d15N_model, temperature_model,d15N_diffu = get_model_data(model_path)
+    if os.path.exists(path_to_model):
+        d15N_model, temperature_model,d15N_diffu = get_model_data(path_to_model)
         cost_fun = cost_func(N_ref, d15N_model)
         print('Temperature', temperature_model)
         print('d15N@CoD', d15N_model)
-    
+
         var_dict['d15N@CoD'][count] = d15N_model
         var_dict['temp'][count] = temperature_model
-    
+
     else:
         print('There is no output file -_- ')
-        #os.chdir('../icecore_data/src/')
         cost_fun = 100.
         print('------------------------------------------------------------------------------------------')
         print('<<<<<<<< Close-off crashed - Setting cost function to 100! >>>>>>>>>>>>>>')
         print('------------------------------------------------------------------------------------------')
-    
+
     var_dict['cost_func'][count] = cost_fun
     count += 1
     var_dict['count'][count] = count
     print('Cost func', cost_fun)
     print('Acc',i_acc[0])
-    
+
     return cost_fun
 '''
 print(N_ref)
@@ -186,16 +188,16 @@ Dist_num =  ['Dist' + str(i) for i in np.arange(4)]### Regime number
 Point_dist_num = np.arange(50)
 
 
-def root_find(path_to_result,N_ref):
+def root_find(path_to_result,N_ref,path_to_model):
     count = 0
-    
+
     var_dict = {'count': np.zeros([N, 1], dtype=int),
                 'd15N@CoD' : np.zeros([N, 1]),
                 'temp' : np.zeros([N, 1]),
                 'cost_func': np.zeros([N, 1])
                 }
 
-    res_c = brentq(func,a = 213,b = 260,args=(N_ref,var_dict),full_output = True,xtol=2e-10,rtol=8.88e-14)
+    res_c = brentq(func,a = 213,b = 260,args=(N_ref,var_dict,path_to_model),full_output = True,xtol=2e-6,rtol=8.88e-10)
     entry_0 = np.where(var_dict['count'] == 0)[0]
     var_dict['count'] = np.delete(var_dict['count'], entry_0[1:])
     var_dict['count'] = var_dict['count'][:-1]
@@ -207,7 +209,7 @@ def root_find(path_to_result,N_ref):
             f[key] = var_dict[key][:max_int]
     f.close()
     var_dict.clear()
-    return None    
+    return None
 
 for i in range(len(Point_N)):
     M = i
@@ -216,60 +218,51 @@ for i in range(len(Point_N)):
     bins2 = np.histogram_bin_edges(Data_d15N, bins='auto')
     n, bins, patches = plt.hist(x=Data_d15N, bins=bins2,alpha=1-0.1*i, rwidth=0.85)
 
-
+model_paths = r'/home/jesperholm/Documents/CommunityFirnThesis-main/Python/CFM/CFM_main/CFMoutput/OptiNoise/CFMresults.hdf5'
 
 model_path = '../CFM/CFM_main/CFMoutput/OptiNoise/CFMresults.hdf5.' ### Insert name
 #results_path = 'resultsFolder/minimizer.h5' #Add name
-folder_path1 = 'resultsFolder/Version1/'
-def Data_crunch(Model,Dist):
+folder_path1 = 'resultsFolder/Version2/'
+def Data_crunch(Model,Dist,path_to_model):
     for i in range(len(Model)):
-        os.chdir('../CFM/CFM_main')
-        file = open('FirnAir_Noise.json')
+        #print(os.getcwd())
+
+        #os.chdir('Optimization')
+        #print(os.getcwd())
+        #os.chdir('CFM/CFM_main')
+        file = open('../CFM/CFM_main/FirnAir_Noise.json')
         data = json.load(file)
         data['physRho'] = str(Model[i])
-            
-        with open("FirnAir_Noise.json", 'w') as f:
+
+        with open("../CFM/CFM_main/FirnAir_Noise.json", 'w') as f:
             json.dump(data, f,indent = 2)
-            
+
             # Closing file
         f.close()
-        os.chdir('../../Optimization')
+        #os.chdir('../../Optimization')
         for j in range(len(Dist)):
-            
+
             s = np.random.normal(Point_N[j],0.02,size=1000)
             d15N_dist = s[(abs(s - s.mean())) < (3 * s.std())][:50]
             print(d15N_dist)
             for k in range(len(d15N_dist)):
                 print(Model[i],Dist[j],k)
                 d15N_ref = d15N_dist[k]
-                folder_path = folder_path1 + str(Model[i]) + '/' + str(Dist[j]) 
+                folder_path = folder_path1 + str(Model[i]) + '/' + str(Dist[j])
                 path = Path(folder_path)
                 path.mkdir(parents=True, exist_ok=True)
                 results_path = folder_path + '/' + 'Point'  + str(k) + '.h5'
                 try:
-                    root_find(results_path,d15N_ref)
-                    
+                    root_find(results_path,d15N_ref,path_to_model)
+
                 except Exception as e: print(e)
-                
+
     return results_path
 
 
 Model_n = ['HLdynamic']
 Dist_n = ['Dist_1']
+#print(os.getcwd())
 
-
-#T = Data_crunch(Model_name, Dist_num)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#os.chdir('../../Optimization')
+T = Data_crunch(Model_name, Dist_num,model_paths)
