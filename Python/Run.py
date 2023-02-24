e29c6a62d778b409ef338635b10580588998340d
Arcaru24601 <Jesper_holm@live.dk>
2023-02-24 16:32:05 +0100

    Run

---
+++ Python/Run.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 24 13:13:30 2023
+
+@author: jespe
+"""
+
+import os
+
+os.system('python main.py')
+os.chdir('Optimization')
+os.system('python Kindler_csv.py')
+os.chdir('home/Documents/CommunityFirnThesis-main/Python/Optimization')
+os.system('python Steady_state.py')
